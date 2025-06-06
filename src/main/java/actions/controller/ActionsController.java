a7b3e8fa1bd8ebdb1ce31e0a3347dc59c1a0b83c
주희 <127922614+ju0204@users.noreply.github.com>
2025-06-06 18:26:14 +0900

    Update ActionsController.java

--- src/main/java/actions/controller/ActionsController.java
+++ src/main/java/actions/controller/ActionsController.java
@@ -9,7 +9,7 @@ public class ActionsController {
 	
 	@GetMapping(value = "/api/actions")
 	public String actionsTest() {
-		return "app-with-actions version:0.0.1";
+		return "app-with-actions version:0.0.2";
 	}
 
 	@GetMapping("/health")
